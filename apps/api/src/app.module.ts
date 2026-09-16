c57e00dd6be2987279977716615f34de5f85f912
Emiliano Alejandro Perez <emiliano.perez@swissmedical.com.ar>
2026-09-16 12:14:30 -0300

    fix: db.module.ts en backend

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -2,13 +2,18 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
-import { DbModule } from './db/db. module';
+import { DbModule } from './db/db.module';
 import { AuthModule } from './auth/auth.module';
 import { HealthController } from './health/health.controller';
 import { NodesModule } from './nodes/nodes.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), DbModule, AuthModule, NodesModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    DbModule,
+    AuthModule,
+    NodesModule,
+  ],
   controllers: [AppController, HealthController],
   providers: [AppService],
 })
